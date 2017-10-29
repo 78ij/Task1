41e7a3c20f436a1bc37105d92ce4b05914340665
Jim Sun <x739566858@outlook.com>
2017-10-29 10:28:31 +0800

    initial

---
+++ Task1/Assets/Scripts/MathUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace UniqueStudioTasks
+{
+    namespace Task1
+    {
+        class MathUtilities
+        {
+            public static float CalculateAngles(Vector3 a)
+            {
+                return Mathf.Atan2(a.y,a.x);
+            }
+        }
+    }
+}
---
+++ Task1/Assets/Scripts/PlaneBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniqueStudioTasks
+{
+    namespace Task1
+    {
+        public abstract class PlaneBase : MonoBehaviour
+        {
+            public abstract void Start();
+            public abstract void Update();
+            public abstract int Health { get; set; }
+        }
+    }
+}
